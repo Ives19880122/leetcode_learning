59c26617abddb561de84a64d8310dd2ca3b7be34
Ives <jianxing.imo99g@gmail.com>
2021-10-13 00:56:30 +0800

    feat: P58 init

---
+++ src/main/java/leetcode/editor/en/P58_LengthOfLastWord.java
@@ -0,0 +1,59 @@
+//Given a string s consisting of some words separated by some number of spaces, 
+//return the length of the last word in the string. 
+//
+// A word is a maximal substring consisting of non-space characters only. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "Hello World"
+//Output: 5
+//Explanation: The last word is "World" with length 5.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "   fly me   to   the moon  "
+//Output: 4
+//Explanation: The last word is "moon" with length 4.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: s = "luffy is still joyboy"
+//Output: 6
+//Explanation: The last word is "joyboy" with length 6.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length <= 10⁴ 
+// s consists of only English letters and spaces ' '. 
+// There will be at least one word in s. 
+// 
+// Related Topics String 👍 214 👎 29
+    
+package leetcode.editor.en;
+/** 
+ * id: 58
+ * title: Length of Last Word
+ * author: Ives.He 
+ * date: 2021-10-13 00:50:06
+ * for TEST copy:
+ * import leetcode.editor.en.P58_LengthOfLastWord.Solution;
+ * P58_LengthOfLastWord_Test
+ */
+public class P58_LengthOfLastWord{
+//leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int lengthOfLastWord(String s) {
+            return  0;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+}
---
+++ src/test/java/leetcode/editor/en/P58_LengthOfLastWord_Test.java
@@ -0,0 +1,13 @@
+package leetcode.editor.en;
+
+import org.junit.Before;
+import leetcode.editor.en.P58_LengthOfLastWord.Solution;
+
+public class P58_LengthOfLastWord_Test {
+    Solution solution;
+
+    @Before
+    public void setup(){
+        solution = new P58_LengthOfLastWord().new Solution();
+    }
+}
