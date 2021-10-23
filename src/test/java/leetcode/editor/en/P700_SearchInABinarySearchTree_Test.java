64480f5a40aadd077322d83fb53565311e6804d2
Ives <jianxing.imo99g@gmail.com>
2021-10-23 22:50:23 +0800

    feat: P700_Search In A Binary Search Tree
    1. version 1 使用簡單遞迴搜尋
    2. 建立java與相關測試
    3. memo:
    0ms,100% / 49Mb,9.85%

---
+++ src/test/java/leetcode/editor/en/P700_SearchInABinarySearchTree_Test.java
@@ -0,0 +1,42 @@
+package leetcode.editor.en;
+
+import leetcode.editor.en.P700_SearchInABinarySearchTree.Solution;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class P700_SearchInABinarySearchTree_Test {
+    Solution solution;
+    @Before
+    public void setup(){
+        this.solution = new P700_SearchInABinarySearchTree().new Solution();
+    }
+    boolean isSameTree(TreeNode l , TreeNode r){
+        return new P100_SameTree().new Solution().isSameTree(l,r);
+    }
+    @Test
+    public void ex1(){
+        TreeNode input = new TreeNode(4,new TreeNode(2,new TreeNode(1),new TreeNode(3)),new TreeNode(7));
+        TreeNode expect = new TreeNode(2,new TreeNode(1),new TreeNode(3));
+        TreeNode output = solution.searchBST(input,2);
+        Assert.assertTrue(isSameTree(expect,output));
+    }
+    @Test
+    public void ex2(){
+        TreeNode input = new TreeNode(4,new TreeNode(2,new TreeNode(1),new TreeNode(3)),new TreeNode(7));
+        TreeNode output = solution.searchBST(input,5);
+        Assert.assertTrue(isSameTree(null,output));
+    }
+    @Test
+    public void tx1(){
+        TreeNode input = new TreeNode(4);
+        TreeNode output = solution.searchBST(input,5);
+        Assert.assertTrue(isSameTree(null,output));
+    }
+    @Test
+    public void tx2(){
+        TreeNode input = new TreeNode(5);
+        TreeNode output = solution.searchBST(input,5);
+        Assert.assertTrue(isSameTree(input,output));
+    }
+}
