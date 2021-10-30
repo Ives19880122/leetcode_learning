befe2dfcb5d1c8711198fe63c54ff927abb61494
Ives <jianxing.imo99g@gmail.com>
2021-10-30 23:10:39 +0800

    init: P24_Swap Nodes In Pairs
    1. 尚未解開

---
+++ src/main/java/leetcode/editor/en/P24_SwapNodesInPairs.java
@@ -0,0 +1,78 @@
+//Given a linked list, swap every two adjacent nodes and return its head. You 
+//must solve the problem without modifying the values in the list's nodes (i.e., 
+//only nodes themselves may be changed.) 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [1,2,3,4]
+//Output: [2,1,4,3]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: head = [1]
+//Output: [1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is in the range [0, 100]. 
+// 0 <= Node.val <= 100 
+// 
+// Related Topics Linked List Recursion 👍 4816 👎 252
+    
+package leetcode.editor.en;
+
+import java.util.List;
+
+/**
+ * id: 24
+ * title: Swap Nodes in Pairs
+ * author: Ives.He 
+ * date: 2021-10-30 21:43:09
+ * for TEST copy:
+ * import leetcode.editor.en.P24_SwapNodesInPairs.Solution;
+ * P24_SwapNodesInPairs_Test
+ */
+public class P24_SwapNodesInPairs{
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        return helper(head);
+    }
+    ListNode helper(ListNode node){
+        if(node == null) {
+            return null;
+        }else{
+            int val = node.next.val;
+            node = node.next;
+            node.next = node.next.next;
+            return new ListNode(val,helper(node));
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/test/java/leetcode/editor/en/P24_SwapNodesInPairs_Test.java
@@ -0,0 +1,27 @@
+package leetcode.editor.en;
+
+import leetcode.editor.en.P24_SwapNodesInPairs.Solution;
+import org.junit.Before;
+import org.junit.Test;
+
+public class P24_SwapNodesInPairs_Test {
+    Solution solution;
+    @Before
+    public void setup(){
+        solution = new P24_SwapNodesInPairs().new Solution();
+    }
+    int i = 0;
+    public ListNode util(int[]nums){
+        while(i<nums.length){
+            int val = nums[i];
+            i++;
+            return new ListNode(val,util(nums));
+        }
+        return null;
+    }
+    @Test
+    public void tx1(){
+        int[] input = {1,2,3,4};
+        solution.swapPairs(util(input));
+    }
+}
