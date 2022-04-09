73ae215d0893b4becc91cb7c4aa4ac569b2bc963
Ives <jianxing.imo99g@gmail.com>
2022-04-09 16:04:01 +0800

    fix: P118_PascalsTriangle
    1. 參考雙迴圈解
    
    memo:
    0ms, 100%
    41.2mb, 79.53%

--- src/main/java/leetcode/editor/en/P118_PascalsTriangle.java
+++ src/main/java/leetcode/editor/en/P118_PascalsTriangle.java
@@ -38,32 +38,40 @@ public class P118_PascalsTriangle{
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        int i =0;
         List<List<Integer>> r = new ArrayList<>();
-        while(i<numRows){
-            r.add(new ArrayList<>());
-            helper(r,i);
-            i++;
+        // 參考調整 雙迴圈
+        for(int i=0; i<numRows;i++){
+            List<Integer> tmp = new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                // 左右端點
+                if(j==0||j==i){
+                    tmp.add(1);
+                // 巴斯卡三角形疊加區域
+                }else{
+                    tmp.add(r.get(i-1).get(j-1)+r.get(i-1).get(j));
+                }
+            }
+            r.add(tmp);
         }
         return r;
     }
-    private void helper(List<List<Integer>> r , int level){
-        List<Integer> ll = r.get(level);
-        if(level==0) {
-            ll.add(1);
-        }else{
-            ll.add(1);
-            // 巴斯卡三角形疊加
-            Iterator<Integer> prev = r.get(level-1).iterator();
-            int sum = prev.next();
-            while(prev.hasNext()){
-                int tmp = prev.next();
-                ll.add(sum+tmp);
-                sum = tmp;
-            }
-            ll.add(1);
-        }
-    }
+//    private void helper(List<List<Integer>> r , int level){
+//        List<Integer> ll = r.get(level);
+//        if(level==0) {
+//            ll.add(1);
+//        }else{
+//            ll.add(1);
+//            // 巴斯卡三角形疊加
+//            Iterator<Integer> prev = r.get(level-1).iterator();
+//            int sum = prev.next();
+//            while(prev.hasNext()){
+//                int tmp = prev.next();
+//                ll.add(sum+tmp);
+//                sum = tmp;
+//            }
+//            ll.add(1);
+//        }
+//    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
