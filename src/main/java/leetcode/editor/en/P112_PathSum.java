456a2e792b1bdc32a763b846392932a88dce4653
Ives <jianxing.imo99g@gmail.com>
2022-04-11 23:37:41 +0800

    fix: P112_PathSum
    1. 參考遞迴,並以疊加方式解題
    
    memo:
    0ms, 100%
    44.5mb, 5.33%

--- src/main/java/leetcode/editor/en/P112_PathSum.java
+++ src/main/java/leetcode/editor/en/P112_PathSum.java
@@ -69,8 +69,10 @@ public class P112_PathSum{
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null) return false;
+        if(root.left != null) root.left.val += root.val;
+        if(root.right != null) root.right.val += root.val;
         if(targetSum == root.val && root.left == null && root.right == null) return true;
-        return hasPathSum(root.left,targetSum - root.val) || hasPathSum(root.right,targetSum - root.val);
+        return hasPathSum(root.left,targetSum) || hasPathSum(root.right,targetSum);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
