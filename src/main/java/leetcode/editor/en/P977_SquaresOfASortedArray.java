987d2a0140396eac0da861ceabd3e44bb5ead774
Ives <jianxing.imo99g@gmail.com>
2022-04-15 00:05:33 +0800

    init: P977_SquaresOfASortedArray
    1. 未優化解
    
    memo:
    701ms, 5.01%
    45.3Mb, 81.24%

---
+++ src/main/java/leetcode/editor/en/P977_SquaresOfASortedArray.java
@@ -0,0 +1,71 @@
+//Given an integer array nums sorted in non-decreasing order, return an array 
+//of the squares of each number sorted in non-decreasing order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [-4,-1,0,3,10]
+//Output: [0,1,9,16,100]
+//Explanation: After squaring, the array becomes [16,1,0,9,100].
+//After sorting, it becomes [0,1,9,16,100].
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [-7,-3,2,3,11]
+//Output: [4,9,9,49,121]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// nums is sorted in non-decreasing order. 
+// 
+//
+// 
+//Follow up: Squaring each element and sorting the new array is very trivial, 
+//could you find an O(n) solution using a different approach? Related Topics Array 
+//Two Pointers Sorting 👍 5100 👎 148
+    
+package leetcode.editor.en;
+/** 
+ * id: 977
+ * title: Squares of a Sorted Array
+ * author: Ives.He 
+ * date: 2022-04-15 00:04:23
+ * for TEST copy:
+ * import leetcode.editor.en.P977_SquaresOfASortedArray .Solution;
+ * P977_SquaresOfASortedArray_Test
+ */
+public class P977_SquaresOfASortedArray{
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int i = 0;
+        int k = i+1;
+        while(i<nums.length){
+            if(k<nums.length){
+                // swap
+                if(nums[i]*nums[i]>nums[k]*nums[k]){
+                    int tmp = nums[k];
+                    nums[k] = nums[i];
+                    nums[i] = tmp;
+                }
+                k++;
+            }else{
+                nums[i] *= nums[i];
+                i++;
+                k=i+1;
+            }
+        }
+        return nums;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
