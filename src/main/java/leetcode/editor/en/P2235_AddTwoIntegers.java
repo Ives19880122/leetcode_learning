c38a1f662679000891c01e7082c9dbd57d9efea9
Ives <jianxing.imo99g@gmail.com>
2022-04-14 21:32:41 +0800

    init: P2235_AddTwoIntegers
    1. 湊題數...
    
    memo:
    0ms, 100%
    39.5mb, 83.03%

---
+++ src/main/java/leetcode/editor/en/P2235_AddTwoIntegers.java
@@ -0,0 +1,47 @@
+//Given two integers num1 and num2, return the sum of the two integers.
+// 
+// Example 1: 
+//
+// 
+//Input: num1 = 12, num2 = 5
+//Output: 17
+//Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is 
+//returned.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: num1 = -10, num2 = 4
+//Output: -6
+//Explanation: num1 + num2 = -6, so -6 is returned.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// -100 <= num1, num2 <= 100 
+// 
+// Related Topics Math 👍 43 👎 185
+    
+package leetcode.editor.en;
+/** 
+ * id: 2235
+ * title: Add Two Integers
+ * author: Ives.He 
+ * date: 2022-04-14 21:29:49
+ * for TEST copy:
+ * import leetcode.editor.en.P2235_AddTwoIntegers .Solution;
+ * P2235_AddTwoIntegers_Test
+ */
+public class P2235_AddTwoIntegers{
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
