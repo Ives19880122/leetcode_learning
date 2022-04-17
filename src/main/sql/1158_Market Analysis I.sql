a046d4e8918c73b08e06211d9e4ff43e6cf30529
Ives <jianxing.imo99g@gmail.com>
2022-04-17 16:26:24 +0800

    fix: 1158_Market Analysis I
    1. 參考加速解, count內可以放case when
    
    memo:
    2141ms , 96.58%
    0mb, 100%

--- src/main/sql/1158_Market Analysis I.sql
+++ src/main/sql/1158_Market Analysis I.sql
@@ -16,4 +16,16 @@ FROM Users
  * Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Market Analysis I.
  */
 
- 
+-- 參考加速解, count內可以放case when
+SELECT 
+  user_id buyer_id, 
+  join_date,
+  count(case when YEAR(order_date)='2019' then order_id end) orders_in_2019
+FROM Users
+LEFT JOIN ORDERS ON buyer_id = user_id
+group by user_id, join_date
+
+/**
+* Runtime: 2141 ms, faster than 96.58% of MS SQL Server online submissions for Market Analysis I.
+* Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Market Analysis I.
+*/
