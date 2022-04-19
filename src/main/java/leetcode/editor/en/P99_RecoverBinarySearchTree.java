40210265df0de5788c7499abfadc4fdd1b130045
Ives <jianxing.imo99g@gmail.com>
2022-04-19 23:03:23 +0800

    init: P99_RecoverBinarySearchTree
    1. inOrder Traversal解題
    2. 搭會會重新排列的priorityQueue
    3. 第二次Traversal時交換
    
    memo:
    7 ms, 10.04%
    47.9 mb, 14.69%

---
+++ src/main/java/leetcode/editor/en/P99_RecoverBinarySearchTree.java
@@ -0,0 +1,90 @@
+//You are given the root of a binary search tree (BST), where the values of 
+//exactly two nodes of the tree were swapped by mistake. Recover the tree without 
+//changing its structure. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: root = [1,3,null,null,2]
+//Output: [3,1,null,null,2]
+//Explanation: 3 cannot be a left child of 1 because 3 > 1. Swapping 1 and 3 
+//makes the BST valid.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = [3,1,4,null,null,2]
+//Output: [2,1,4,null,null,3]
+//Explanation: 2 cannot be in the right subtree of 3 because 2 < 3. Swapping 2 
+//and 3 makes the BST valid.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [2, 1000]. 
+// -2³¹ <= Node.val <= 2³¹ - 1 
+// 
+//
+// 
+//Follow up: A solution using O(n) space is pretty straight-forward. Could you 
+//devise a constant O(1) space solution? Related Topics Tree Depth-First Search 
+//Binary Search Tree Binary Tree 👍 4858 👎 168
+    
+package leetcode.editor.en;
+
+import leetcode.util.TreeNode;
+
+import java.util.PriorityQueue;
+
+/**
+ * id: 99
+ * title: Recover Binary Search Tree
+ * author: Ives.He 
+ * date: 2022-04-19 22:54:16
+ * for TEST copy:
+ * import leetcode.editor.en.P99_RecoverBinarySearchTree .Solution;
+ * P99_RecoverBinarySearchTree_Test
+ */
+public class P99_RecoverBinarySearchTree{
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean hasTravel = false;
+    private final PriorityQueue<Integer> queue = new PriorityQueue<>();
+    public void recoverTree(TreeNode root) {
+        inOrder(root);
+        this.hasTravel=true;
+        inOrder(root);
+    }
+    void inOrder(TreeNode node){
+        if(node==null)return;
+        inOrder(node.left);
+        if(!hasTravel){
+            this.queue.offer(node.val);
+        }else if(!queue.isEmpty()){
+            node.val = this.queue.poll();
+        }
+        inOrder(node.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
