12c49a384271ce45e62ebacb15c0b37d3078b529
Ives <jianxing.imo99g@gmail.com>
2022-04-27 01:20:10 +0800

    fix: P187_RepeatedDnaSequences
    1. 改用set
    
    memo:
    37ms, 44.91%
    63.8mb, 48.77%

--- src/main/java/leetcode/editor/en/P187_RepeatedDnaSequences.java
+++ src/main/java/leetcode/editor/en/P187_RepeatedDnaSequences.java
@@ -32,10 +32,7 @@
     
 package leetcode.editor.en;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -55,14 +52,13 @@ class Solution {
         if(s.length()<=10) return list;
         int start = 0;
         int end = 10;
-        HashMap<String,Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
         // 調整操作
         while(end<=s.length()){
             String key = s.substring(start++,end++);
-            if(!map.containsKey(key)){
-                map.put(key,1);
-            }else if(map.get(key)==1){
-                map.put(key,2);
+            if(!set.contains(key)){
+                set.add(key);
+            }else if(!list.contains(key)){
                 list.add(key);
             }
         }
