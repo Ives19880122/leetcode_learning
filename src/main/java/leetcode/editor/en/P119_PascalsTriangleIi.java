a282662c4f45fbe4d2c022961dbf6c9673771d01
Ives <jianxing.imo99g@gmail.com>
2022-09-16 00:05:34 +0800

    feat: P119_PascalsTriangleIi
    1. 遞迴取得前一層
    2. 疊加到新一層
    3. 使用兩個List, 考慮優化
    
    memo:
    3ms 22.67%,
    41.6MB 63.34%

---
+++ src/main/java/leetcode/editor/en/P119_PascalsTriangleIi.java
@@ -0,0 +1,67 @@
+//Given an integer rowIndex, return the rowIndexᵗʰ (0-indexed) row of the 
+//Pascal's triangle. 
+//
+// In Pascal's triangle, each number is the sum of the two numbers directly 
+//above it as shown: 
+//
+// 
+// Example 1: 
+// Input: rowIndex = 3
+//Output: [1,3,3,1]
+// Example 2: 
+// Input: rowIndex = 0
+//Output: [1]
+// Example 3: 
+// Input: rowIndex = 1
+//Output: [1,1]
+// 
+// 
+// Constraints: 
+//
+// 
+// 0 <= rowIndex <= 33 
+// 
+//
+// 
+// Follow up: Could you optimize your algorithm to use only O(rowIndex) extra 
+//space? 
+// Related Topics Array Dynamic Programming 👍 3127 👎 279
+    
+package leetcode.editor.en;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * id: 119
+ * title: Pascal's Triangle II
+ * author: Ives.He 
+ * date: 2022-09-15 23:43:13
+ * for TEST copy:
+ * import leetcode.editor.en.P119_PascalsTriangleIi .Solution;
+ * P119_PascalsTriangleIi_Test
+ */
+public class P119_PascalsTriangleIi{
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        if(rowIndex==0) return Arrays.asList(1);
+        if(rowIndex==1) return Arrays.asList(1,1);
+        List<Integer> pre = getRow(rowIndex-1);
+        List<Integer> cur = new ArrayList<>();
+        cur.add(1);     // head
+        int l = 0;
+        int r = 1;
+        while(r<pre.size()){
+            cur.add(pre.get(l)+pre.get(r));
+            l++;
+            r++;
+        }
+        cur.add(1);     // tail
+        return cur;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
