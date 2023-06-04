0cafe172e0d86cdb428ebeef325efef4a4a241df
Ives <jianxing.imo99g@gmail.com>
2023-06-04 17:02:07 +0800

    feat: P222_CountCompleteTreeNodes
    1. 使用遞迴計算左右樹的數量
    
    memo:
    0 ms, 100%
    45.7 MB, 32.35%

---
+++ src/main/java/leetcode/editor/en/P222_CountCompleteTreeNodes.java
@@ -0,0 +1,86 @@
+//Given the root of a complete binary tree, return the number of the nodes in 
+//the tree. 
+//
+// According to Wikipedia, every level, except possibly the last, is completely 
+//filled in a complete binary tree, and all nodes in the last level are as far 
+//left as possible. It can have between 1 and 2ʰ nodes inclusive at the last level h.
+// 
+//
+// Design an algorithm that runs in less than O(n) time complexity. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: root = [1,2,3,4,5,6]
+//Output: 6
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = []
+//Output: 0
+// 
+//
+// Example 3: 
+//
+// 
+//Input: root = [1]
+//Output: 1
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [0, 5 * 10⁴]. 
+// 0 <= Node.val <= 5 * 10⁴ 
+// The tree is guaranteed to be complete. 
+// 
+//
+// Related Topics Binary Search Tree Depth-First Search Binary Tree 👍 7473 👎 4
+//04
+    
+package leetcode.editor.en;
+
+import leetcode.util.TreeNode;
+
+/**
+ * id: 222
+ * title: Count Complete Tree Nodes
+ * author: Ives.He 
+ * date: 2023-06-04 16:59:38
+ * for TEST copy:
+ * import leetcode.editor.en.P222_CountCompleteTreeNodes .Solution;
+ * P222_CountCompleteTreeNodes_Test
+ */
+public class P222_CountCompleteTreeNodes{
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root==null) return 0;
+        int count = 1;
+        count += countNodes(root.left);
+        count += countNodes(root.right);
+        return count;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
